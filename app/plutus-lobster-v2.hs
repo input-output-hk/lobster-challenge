40e56db6291bd503bc444fb1906668936ee9bc5b
Lars Brünjes <brunjlar@gmail.com>
2021-09-17 00:06:11 +0200

    first concurrent version

--- app/plutus-lobster-v2.hs
+++ app/plutus-lobster-v2.hs
@@ -9,13 +9,14 @@ import Cardano.PlutusLobster.LobsterV2Script
 
 main :: IO ()
 main = do
-    [nftSymbol, otherSymbol, seed', nameCount', voteCount'] <- getArgs
+    [nftSymbol, otherSymbol, seed', nameCount', voteCount', fee'] <- getArgs
     let seed      = read seed'
         nft       = AssetClass (fromString nftSymbol,   nftTokenName)
         counter   = AssetClass (fromString otherSymbol, counterTokenName)
         votes     = AssetClass (fromString otherSymbol, votesTokenName)
         nameCount = read nameCount'
         voteCount = read voteCount'
+        fee       = read fee'
         lp          = LobsterParams
             { lpSeed      = seed
             , lpNFT       = nft
@@ -23,11 +24,18 @@ main = do
             , lpVotes     = votes
             , lpNameCount = nameCount
             , lpVoteCount = voteCount
+            , lpFee       = fee
             }
         lobsterFile = "scripts/lobster-v2.plutus"
+        requestFile = "scripts/lobster-request.plutus"
     print lp
 
     lobsterResult <- writeFileTextEnvelope lobsterFile Nothing $ apiLobsterScript lp
     case lobsterResult of
         Left err -> print $ displayError err
         Right () -> putStrLn $ "wrote lobster script to file " ++ lobsterFile
+
+    requestResult <- writeFileTextEnvelope requestFile Nothing $ apiRequestScript lp
+    case requestResult of
+        Left err -> print $ displayError err
+        Right () -> putStrLn $ "wrote request script to file " ++ requestFile
--- src/Cardano/PlutusLobster/LobsterV2Script.hs
+++ src/Cardano/PlutusLobster/LobsterV2Script.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -15,6 +16,7 @@
 
 module Cardano.PlutusLobster.LobsterV2Script
   ( apiLobsterScript
+  , apiRequestScript
   , LobsterParams (..)
   ) where
 
@@ -25,6 +27,7 @@ import qualified Data.ByteString.Short    as SBS
 import           Ledger                   hiding (singleton)
 import qualified Ledger.Typed.Scripts     as Scripts
 import           Ledger.Value             as Value
+import           Ledger.Ada
 import qualified PlutusTx
 import           PlutusTx.Builtins        (modInteger)
 import           PlutusTx.Prelude         hiding (Semigroup (..), unless)
@@ -32,34 +35,67 @@ import qualified Plutus.V1.Ledger.Scripts as Plutus
 import           Prelude                  (Show)
 
 data LobsterParams = LobsterParams
-    { lpSeed      :: Integer
-    , lpNFT       :: AssetClass
-    , lpCounter   :: AssetClass
-    , lpVotes     :: AssetClass
-    , lpNameCount :: Integer
-    , lpVoteCount :: Integer
+    { lpSeed      :: !Integer
+    , lpNFT       :: !AssetClass
+    , lpCounter   :: !AssetClass
+    , lpVotes     :: !AssetClass
+    , lpNameCount :: !Integer
+    , lpVoteCount :: !Integer
+    , lpFee       :: !Integer
     } deriving Show
 
 PlutusTx.makeLift ''LobsterParams
 
 {- HLINT ignore "Avoid lambda" -}
 
+mkRequestValidator :: LobsterParams -> BuiltinData -> BuiltinData -> ScriptContext -> Bool
+mkRequestValidator lp _ _ ctx =
+    traceIfFalse "lobster input missing" $
+        any (\i -> assetClassValueOf (txOutValue $ txInInfoResolved i) (lpNFT lp) == 1) $ txInfoInputs $ scriptContextTxInfo ctx
+
+data Requesting
+instance Scripts.ValidatorTypes Requesting where
+    type instance DatumType Requesting = BuiltinData
+    type instance RedeemerType Requesting = BuiltinData
+
+typedRequestValidator :: LobsterParams -> Scripts.TypedValidator Requesting
+typedRequestValidator lp = Scripts.mkTypedValidator @Requesting
+    ($$(PlutusTx.compile [|| mkRequestValidator ||])
+        `PlutusTx.applyCode` PlutusTx.liftCode lp)
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @BuiltinData @BuiltinData
+
+requestValidator :: LobsterParams -> Validator
+requestValidator = Scripts.validatorScript . typedRequestValidator
+
+requestScript :: LobsterParams -> Plutus.Script
+requestScript = Ledger.unValidatorScript . requestValidator
+
+requestScriptAsShortBs :: LobsterParams -> SBS.ShortByteString
+requestScriptAsShortBs = SBS.toShort . LB.toStrict . serialise . requestScript
+
+apiRequestScript :: LobsterParams -> PlutusScript PlutusScriptV1
+apiRequestScript = PlutusScriptSerialised . requestScriptAsShortBs
+
 expectedDatumHash :: DatumHash
 expectedDatumHash = DatumHash "03170a2e7597b7b7e3d84c05391d139a62b157e78786d8c082f29dcf4c111314" -- hash of 0
 
-mkLobsterValidator :: DatumHash -> LobsterParams -> Integer -> BuiltinData -> ScriptContext -> Bool
-mkLobsterValidator h lp d _ ctx
+mkLobsterValidator :: Address -> DatumHash -> LobsterParams -> Integer -> BuiltinData -> ScriptContext -> Bool
+mkLobsterValidator addr h lp _ _ ctx
     | oldNFT == 1 =
-        traceIfFalse "input datum must be zero"  (d == 0)                             &&
-        traceIfFalse "output datum must be zero" (txOutDatumHash ownOutput == Just h) &&
-        traceIfFalse "NFT missing from output"   (newNFT   == 1)                      &&
-        traceIfFalse "already finished"          (oldVotes <= lpVoteCount lp)         &&
-        traceIfFalse "wrong new votes"           (newVotes == oldVotes + 1)           &&
-        if oldVotes < lpVoteCount lp then
-            traceIfFalse "counter increase too small" (increase >= 1)                                                        &&
-            traceIfFalse "counter increase too large" (increase <= 100)
-        else
-            traceIfFalse "wrong counter value"        (newCounter == ((lpSeed lp + oldCounter) `modInteger` lpNameCount lp))
+        traceIfFalse "output datum must be zero" (txOutDatumHash ownOutput == Just h)       &&
+        traceIfFalse "NFT missing from output"   (newNFT   == 1)                            &&
+        if | oldVotes < lpVoteCount lp  ->
+                traceIfFalse "wrong new counter" (newCounter == oldCounter + sum requests)  &&
+                traceIfFalse "wrong new votes"   (newVotes   == oldVotes + length requests) &&
+                traceIfFalse "too many votes"    (newVotes   <= lpVoteCount lp)
+           | oldVotes == lpVoteCount lp ->
+                traceIfFalse "wrong new counter" (newCounter == finalCounter)               &&
+                traceIfFalse "wrong new votes"   (newVotes   == 1 + lpVoteCount lp)
+           | otherwise                  ->
+                traceIfFalse "wrong new counter" (newCounter == oldCounter)                 &&
+                traceIfFalse "wrong new votes"   (newVotes   == oldVotes)
     | otherwise   = True -- If we don't have the UTxO with the NFT, we don't care.
   where
     ownInput :: TxOut
@@ -76,14 +112,29 @@ mkLobsterValidator h lp d _ ctx
     inVal = txOutValue ownInput
     outVal = txOutValue ownOutput
 
-    oldNFT, newNFT, oldCounter, newCounter, increase, oldVotes, newVotes :: Integer
-    oldNFT     = assetClassValueOf inVal  $ lpNFT lp
-    newNFT     = assetClassValueOf outVal $ lpNFT lp
-    oldCounter = assetClassValueOf inVal  $ lpCounter lp
-    newCounter = assetClassValueOf outVal $ lpCounter lp
-    oldVotes   = assetClassValueOf inVal  $ lpVotes lp
-    newVotes   = assetClassValueOf outVal $ lpVotes lp
-    increase   = newCounter - oldCounter
+    oldNFT, newNFT, oldCounter, newCounter, oldVotes, newVotes, finalCounter :: Integer
+    oldNFT       = assetClassValueOf inVal  $ lpNFT lp
+    newNFT       = assetClassValueOf outVal $ lpNFT lp
+    oldCounter   = assetClassValueOf inVal  $ lpCounter lp
+    newCounter   = assetClassValueOf outVal $ lpCounter lp
+    oldVotes     = assetClassValueOf inVal  $ lpVotes lp
+    newVotes     = assetClassValueOf outVal $ lpVotes lp
+    finalCounter = (lpSeed lp + oldCounter) `modInteger` lpNameCount lp
+
+    lovelace :: AssetClass
+    lovelace = AssetClass (adaSymbol, adaToken)
+
+    requests :: [Integer]
+    requests = [ c
+               | i <- txInfoInputs $ scriptContextTxInfo ctx
+               , let o = txInInfoResolved i
+               , txOutAddress o == addr
+               , let v = txOutValue o
+               , let l = assetClassValueOf v lovelace
+               , l >= lpFee lp
+               , let c = assetClassValueOf v $ lpCounter lp
+               , c >= 1 && c <= 100 -- vote in valid range 1 - 100
+               ]
 
 data LobsterNaming
 instance Scripts.ValidatorTypes LobsterNaming where
@@ -93,6 +144,7 @@ instance Scripts.ValidatorTypes LobsterNaming where
 typedLobsterValidator :: LobsterParams -> Scripts.TypedValidator LobsterNaming
 typedLobsterValidator lp = Scripts.mkTypedValidator @LobsterNaming
     ($$(PlutusTx.compile [|| mkLobsterValidator ||])
+        `PlutusTx.applyCode` PlutusTx.liftCode (scriptAddress $ requestValidator lp)
         `PlutusTx.applyCode` PlutusTx.liftCode expectedDatumHash
         `PlutusTx.applyCode` PlutusTx.liftCode lp)
     $$(PlutusTx.compile [|| wrap ||])
